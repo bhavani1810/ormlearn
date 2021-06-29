e460440106fbd32fecdb60d3481f971ee2acc092
bhavani1810 <56405826+bhavani1810@users.noreply.github.com>
2021-06-29 09:46:04 +0530

    committed

---
+++ src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,24 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+
+import com.cognizant.ormlearn.custom_exception.CountryNotFoundException;
+import com.cognizant.ormlearn.model.Country;
+
+public interface CountryService {
+
+	public List<Country> getAllCountries();
+
+	public Country findCountryByCode(String countryCode) throws CountryNotFoundException;
+
+	public void addCountry(Country country);
+
+
+	void deleteCountry(String code);
+	
+	List<Country> findCountryByCharacter(String name);
+
+	List<Country> findCountryUsingSingleCharacter(String name);
+
+	
+}
